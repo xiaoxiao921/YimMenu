b9ba4933980077fa4cbdd34befa2802902ea6784
xiaoxiao921 <xiaoxiao921@hotmail.fr>
2024-02-28 00:16:21 +0100

    fast quit but it's actually fast

--- src/backend/commands/system/fast_quit.cpp
+++ src/backend/commands/system/fast_quit.cpp
@@ -8,7 +8,7 @@ namespace big
 
 		virtual void execute(const command_arguments&, const std::shared_ptr<command_context> ctx) override
 		{
-			exit(0);
+			TerminateProcess(GetCurrentProcess(), 0);
 		}
 	};
 
